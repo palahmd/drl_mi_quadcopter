883d04a9b4546cefb446875c5ee5f053423fc552
Pala Ahmed <pala.ahmed@gmail.com>
2020-10-08 19:36:27 +0200

    small bugs fixed

--- scripts/c++/spawn_quadcopter.cpp
+++ scripts/c++/spawn_quadcopter.cpp
@@ -5,8 +5,8 @@ int main(int argc, char* argv[]) {
   auto binaryPath = raisim::Path::setFromArgv(argv[0]);
   raisim::World::setActivationKey( binaryPath.getDirectory() + "\\rsc\\activation.raisim");
   raisim::World world;
-  auto quadcopter = world.addArticulatedSystem(binaryPath.getDirectory() + "\\rsc\\quadcopter\\hummindbird\\urdf\\hummingbird.urdf");
-  auto ground = world.addGround();
+  auto quadcopter = world.addArticulatedSystem(binaryPath.getDirectory() + "\\rsc\\quadcopter\\Hummingbird\\urdf\\hummingbird.urdf");
+  auto ground = world.addGround(-1);
   world.setTimeStep(0.002);
 
   /// launch raisim server for visualization. Can be visualized on raisimUnity
