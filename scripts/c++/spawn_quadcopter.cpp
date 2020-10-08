daedd900bd86ec1fd31e8b39459b874baa68e92c
Pala Ahmed <pala.ahmed@gmail.com>
2020-10-08 17:31:47 +0200

    python implementation of spawn_quadcopter

---
+++ scripts/c++/spawn_quadcopter.cpp
@@ -0,0 +1,24 @@
+#include "raisim/World.hpp"
+#include "raisim/RaisimServer.hpp"
+
+int main(int argc, char* argv[]) {
+  auto binaryPath = raisim::Path::setFromArgv(argv[0]);
+  raisim::World::setActivationKey( binaryPath.getDirectory() + "\\rsc\\activation.raisim");
+  raisim::World world;
+  auto quadcopter = world.addArticulatedSystem(binaryPath.getDirectory() + "\\rsc\\quadcopter\\hummindbird\\urdf\\hummingbird.urdf");
+  auto ground = world.addGround();
+  world.setTimeStep(0.002);
+
+  /// launch raisim server for visualization. Can be visualized on raisimUnity
+  raisim::RaisimServer server(&world);
+  server.launchServer();
+  server.focusOn(quadcopter);
+
+  while (1) {
+    raisim::MSLEEP(2);
+    server.integrateWorldThreadSafe();
+    
+  }
+
+  server.killServer();
+}
