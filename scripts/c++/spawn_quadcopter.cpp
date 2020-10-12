4071d18443d7dfc61adb759ff8ca1e5c4bb634d9
Pala Ahmed <pala.ahmed@gmail.com>
2020-10-12 21:06:24 +0200

    added single body spawn version

--- scripts/c++/spawn_quadcopter.cpp
+++ scripts/c++/spawn_quadcopter.cpp
@@ -1,5 +1,6 @@
 #include "raisim/World.hpp"
 #include "raisim/RaisimServer.hpp"
+#include "Eigen/Dense"
 
 int main(int argc, char* argv[]) {
   auto binaryPath = raisim::Path::setFromArgv(argv[0]);
@@ -7,6 +8,11 @@ int main(int argc, char* argv[]) {
   raisim::World world;
   auto quadcopter = world.addArticulatedSystem(binaryPath.getDirectory() + "\\rsc\\quadcopter\\Hummingbird\\urdf\\hummingbird.urdf");
   auto ground = world.addGround(-1);
+  Eigen::VectorXd init_State;
+  init_State << 0,0,50, 1, 0, 0, 0, 0, 0, 0;
+  Eigen::VectorXd init_Vel;
+  init_Vel << 0,0,10, 0, 0, 0, 0, 0, 0, 0;
+  quadcopter->setState(init_State, init_Vel);
   world.setTimeStep(0.002);
 
   /// launch raisim server for visualization. Can be visualized on raisimUnity
--- scripts/python/spawn_quadcopter.py
+++ scripts/python/spawn_quadcopter.py
@@ -5,56 +5,26 @@ import time
 
 raisim.World.set_license_file(os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/activation.raisim")
 quadcopter_urdf_file = os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/quadcopter/Hummingbird/urdf/hummingbird.urdf"
-dummy_inertia = np.zeros([3, 3])
-np.fill_diagonal(dummy_inertia, 0.1)
 
-world = raisim.World()
 world.set_time_step(0.001)
 server = raisim.RaisimServer(world)
 ground = world.add_ground()
 
-anymal = world.add_articulated_system(anymal_urdf_file)
-anymal.set_name("Hummingbird")
-anymal_nominal_joint_config = np.array([0, -1.5, 0.54, 1.0, 0.0, 0.0, 0.0, 0.03, 0.4, -0.8,
-                                        -0.03, 0.4, -0.8, 0.03, -0.4, 0.8, -0.03, -0.4, 0.8])
-anymal.set_generalized_coordinates(anymal_nominal_joint_config)
-anymal.set_pd_gains(200*np.ones([18]), np.ones([18]))
-anymal.set_pd_targets(anymal_nominal_joint_config, np.zeros([18]))
+quadcopter = world.add_articulated_system(quadcopter_urdf_file)
+quadcopter.set_name("Hummingbird")
+quadcopter_nominal_joint_config = np.array([0, 0, 0.063, 1.0, 0.0, 0.0, 1.0, 0, 0, 0, 0])
+quadcopter.set_generalized_coordinates(quadcopter_nominal_joint_config)
 
 
 server.launch_server(8080)
 
-for i in range(5):
-    for j in range(5):
-        object_type = (i + j*6) % 5
-
-        if object_type == 0:
-            obj = world.add_mesh(monkey_file, 5.0, dummy_inertia, np.array([0, 0, 0]), 0.3)
-        elif object_type == 1:
-            obj = world.add_cylinder(0.2, 0.3, 2.0)
-        elif object_type == 2:
-            obj = world.add_capsule(0.2, 0.3, 2.0)
-        elif object_type == 3:
-            obj = world.add_box(0.4, 0.4, 0.4, 2.0)
-        else:
-            obj = world.add_sphere(0.3, 2.0)
-
-        obj.set_position(i-2.5, j-2.5, 5)
 
 time.sleep(2)
 world.integrate1()
 
-### get dynamic properties
-# mass matrix
-mass_matrix = anymal.get_mass_matrix()
+print(quadcopter.get_mass_matrix())
 
-# non-linear term (gravity+coriolis)
-non_linearities = anymal.get_non_linearities()
-
-# non-linear term (gravity+coriolis)
-non_linearities = anymal.get_non_linearities()
-
-for i in range(50000):
+for i in range(5000000):
     world.integrate()
 
 server.kill_server()
