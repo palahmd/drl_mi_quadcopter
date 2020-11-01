e7590ae7680b006514f5b8c661b8b85a9315ff79
Pala Ahmed <pala.ahmed@gmail.com>
2020-11-01 22:09:22 +0100

    fast testing of gyrocscopic effect

--- raisim/src/gyroscopic_effect.cpp
+++ raisim/src/gyroscopic_effect.cpp
@@ -4,22 +4,21 @@
 #include "iostream"
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     auto binaryPath = raisim::Path::setFromArgv(argv[0]);
-    raisim::World::setActivationKey(binaryPath.getDirectory() + "\\rsc\\activation.raisim");
+    raisim::World::setActivationKey( binaryPath.getDirectory() + "\\rsc\\activation.raisim");
     raisim::World world;
     world.setTimeStep(0.002); // what does timestep do and why does the sim. go slower when timestep is changed
 
     /// create raisim objects
     auto ground = world.addGround();
-    auto quadcopter = world.addArticulatedSystem(
-            binaryPath.getDirectory() + "\\rsc\\quadcopter\\ITM_Quadrocopter\\urdf\\ITM_Quadrocopter.urdf");
+    auto quadcopter = world.addArticulatedSystem(binaryPath.getDirectory() + "\\rsc\\quadcopter\\ITM_Quadrocopter\\urdf\\ITM_Quadrocopter.urdf");
     quadcopter->setName("Quaddy");
 
     //quadcopter model parameters
     const double rotorPos = 0.17104913036744201, momentConst = 0.016;
-    const double k_f = 6.11 * pow(10, -8), k_m = 1.5 * pow(10, -9); //in rpm²
-    const double rps = 2 * M_PI, rps2rpm = rps * 60;
+    const double k_f = 6.11*pow(10,-8), k_m = 1.5*pow(10,-9); //in rpm²
+    const double rps = 2*M_PI, rpm = rps/60;
 
     // initialize general coordinates, velocities and number of rotors
     int gcDim = quadcopter->getGeneralizedCoordinateDim();
@@ -28,11 +27,12 @@ int main(int argc, char *argv[]) {
 
     /// initialize containers
     Eigen::VectorXd gc_init, gv_init, gc, gv, pTarget, pTarget12, vTarget;
-    gc.setZero(gcDim); gc_init.setZero(gcDim); gv.setZero(gvDim); gv_init.setZero(gvDim);
+    gc.setZero(gcDim); gc_init.setZero(gcDim);
+    gv.setZero(gvDim); gv_init.setZero(gvDim);
     pTarget.setZero(gcDim); vTarget.setZero(gvDim); pTarget12.setZero(nRotors);
 
     /// nominal configuration of quadcopter: [0]-[2]: center of mass, [3]-[6]: quanternions, [7]-[10]: rotors
-    gc_init << 0, 0, 0.1433, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+    gc_init << 0, 0, 10.1433, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
     gv_init << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
     quadcopter->setState(gc_init, gv_init);
 
@@ -44,6 +44,11 @@ int main(int argc, char *argv[]) {
             rotorPos, -rotorPos, -rotorPos, rotorPos,
             momentConst, momentConst, momentConst, momentConst,
             1, 1, 1, 1;
+    vTarget = thrusts.cast<double>();
+    Eigen::Vector4d testvector;
+    testvector << 1, 2, 3, 4;
+
+    std::cout << testvector.segment(0,3) << std::endl;
 
     /// set initial state and dynamics properties
     quadcopter->setIntegrationScheme(raisim::ArticulatedSystem::IntegrationScheme::SEMI_IMPLICIT);
@@ -56,20 +61,33 @@ int main(int argc, char *argv[]) {
     server.launchServer();
     server.focusOn(quadcopter);
 
-    for (int i = 0; i < 20000; i++) {
+    size_t f = 0;
+    for (int i=0; i<20000; i++){
+//    quadcopter->setExternalForce(1, raisim::ArticulatedSystem::Frame::BODY_FRAME,
+//                                 {0, 0, 5}, raisim::ArticulatedSystem::Frame::BODY_FRAME, {0, 0, 0});
+//    quadcopter->setExternalForce(2, raisim::ArticulatedSystem::Frame::BODY_FRAME,
+//                                   {0, 0, 5}, raisim::ArticulatedSystem::Frame::BODY_FRAME, {0, 0, 0});
+//    quadcopter->setExternalForce(3, raisim::ArticulatedSystem::Frame::BODY_FRAME,
+//                                   {0, 0, 6}, raisim::ArticulatedSystem::Frame::BODY_FRAME, {0, 0, 0});
+//    quadcopter->setExternalForce(4, raisim::ArticulatedSystem::Frame::BODY_FRAME,
+//                                   {0, 0, 5}, raisim::ArticulatedSystem::Frame::BODY_FRAME, {0, 0, 0});
 
-        // applying forces at every rotor in rotor frame
-        quadcopter->setExternalForce(1, raisim::ArticulatedSystem::Frame::BODY_FRAME,
-                                     {0, 0, 2.4}, raisim::ArticulatedSystem::Frame::BODY_FRAME, {0, 0, 0});
-        quadcopter->setExternalForce(2, raisim::ArticulatedSystem::Frame::BODY_FRAME,
-                                     {0, 0, 2.4}, raisim::ArticulatedSystem::Frame::BODY_FRAME, {0, 0, 0});
-        quadcopter->setExternalForce(3, raisim::ArticulatedSystem::Frame::BODY_FRAME,
-                                     {0, 0, 2.4}, raisim::ArticulatedSystem::Frame::BODY_FRAME, {0, 0, 0});
-        quadcopter->setExternalForce(4, raisim::ArticulatedSystem::Frame::BODY_FRAME,
-                                     {0, 0, 2}, raisim::ArticulatedSystem::Frame::BODY_FRAME, {0, 0, 0});
+        // gyroscopic effect is less than 0.01 Nm
+        quadcopter->getState(gc, gv);
+        gv[0] = 0, gv[1] = 0, gv[2] = 0, gv[3] = 0, gv[5] = 0;
 
+        if (f>1) {
+
+            gv[6] = 10000*rpm;
+            gv[7] = -10000*rpm;
+            gv[8] = 10000*rpm;
+            gv[9] = -10000*rpm;
+            quadcopter->setExternalTorque(0, {0, 0.01, 0});
+        }
+        quadcopter->setGeneralizedVelocity(gv);
         raisim::MSLEEP(2);
         server.integrateWorldThreadSafe();
+        f++;
     }
 
     server.killServer();
