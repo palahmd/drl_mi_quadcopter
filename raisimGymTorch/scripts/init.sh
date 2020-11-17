a5f27688fd2f6854b9151c08986a3dcb91c5e262
Pala Ahmed <pala.ahmed@gmail.com>
2020-11-17 14:35:22 +0100

    added shell scripts

---
+++ raisimGymTorch/scripts/init.sh
@@ -0,0 +1,18 @@
+echo 'Path where raisimlib is installed, beginning from "/home". Example: /home/USER/raisimlib:'
+
+read raisim_path
+
+RSG_PATH=$raisim_path'/raisimGymTorch'
+
+echo '# raisimGymTorch Path' >> ~/.bashrc
+echo export RSG_PATH=$raisim_path'/raisimGymTorch' >> ~/.bashrc
+
+source ~/.bashrc
+
+cd ..
+this_path=`pwd`
+
+mkdir $RSG_PATH/raisimGymTorch/env/envs/rsg_quadcopter
+ln -s $this_path/rsg_quadcopter/* $RSG_PATH/raisimGymTorch/env/envs/rsg_quadcopter/
+
+echo RSG_PATH=$raisim_path'/raisimGymTorch' added to ~/.bashrc and rsg_quacopter files linked to raisimGymTorch. Run compile.sh once and after any changes in rsg_quadcopter files before running a python script.
