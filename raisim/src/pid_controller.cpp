75c44d082440d5c46e5e6552abfeaffcbd2c5048
tomcattigerkkk <wei.luo.88@outlook.com>
2021-05-21 00:41:05 +0200

    create and update a new braunch

--- raisim/src/pid_controller.cpp
+++ raisim/src/pid_controller.cpp
@@ -13,64 +13,64 @@ pidController::pidController(double P, double I, double D) {
 
     currState.setZero(12); //pos, linVel, angVel, eulerAngles
     desState.setZero(12); // != desired Position which is the target point
-    errState.setZero(12);
+    errState.setZero(13);
     controlThrusts.setZero();
 }
 
 void pidController::smallAnglesControl() {
-    /*** PD Controller for small angles ***/
-    // current state and error. eulerAngles is a conversion from quaternions to Euler Angles
-    eulerAngles = ToEulerAngles(ob_q);
-    angVel_Body = rot.e().inverse() * angVel_W.e();
-    currState << pos.e(), eulerAngles, linVel_W.e(), angVel_Body;
-    desState = targetPoint;
-    errState = targetPoint - currState;
+    /*** pd controller for small angles ***/
+    // current state and error. eulerangles is a conversion from quaternions to euler angles
+    eulerangles = toeulerangles(ob_q);
+    angvel_body = rot.e().inverse() * angvel_w.e();
+    currstate << pos.e(), eulerangles, linvel_w.e(), angvel_body;
+    desstate = targetpoint;
+    errstate = targetpoint - currstate;
 
-    /** outer PID controller for Position Control, runs with 20 Hz
+    /** outer pid controller for position control, runs with 20 hz
      ** input: error between target point and current state
      ** output: desired acceleration, desired pitch and roll angles and u[0] for altitude control **/
-    if (loopCount > 4) {
-        desAcc = pGain * errState.segment(0, 3) + dGain * errState.segment(6, 3);
-        desState[3] = 1 / g * (desAcc[0] * sin(currState[5]) - desAcc[1] * cos(currState[5]));
-        desState[4] = 1 / g * (desAcc[0] * cos(currState[5]) + desAcc[1] * sin(currState[5]));
-        u[0] = m * g + m * desAcc[2] + m * iGain * errState[2] * timeStep;
+    if (loopcount > 4) {
+        desacc = pgain * errstate.segment(0, 3) + dgain * errstate.segment(6, 3);
+        desstate[3] = 1 / g * (desacc[0] * sin(currstate[5]) - desacc[1] * cos(currstate[5]));
+        desstate[4] = 1 / g * (desacc[0] * cos(currstate[5]) + desacc[1] * sin(currstate[5]));
+        u[0] = m * g + m * desacc[2] + m * igain * errstate[2] * timestep;
     } else {
         u[0] = u[0];
     }
 
-    /** inner PD controller for Attitude Control, runs with 100 Hz
+    /** inner pd controller for attitude control, runs with 100 hz
      ** input: desired acceleration and error between desired state and current state
      ** output: u[1] - u[3] for attitude control **/
-    errState = desState - currState;
-    u[1] = inertiaDiagVec[0] * 81 * errState[3] + 2 * inertiaDiagVec[0] * 9 * errState[9];
-    u[2] = inertiaDiagVec[1] * 81 * errState[4] + 2 * inertiaDiagVec[1] * 9 * errState[10];
-    u[3] = inertiaDiagVec[2] * 81 * errState[5] + 2 * inertiaDiagVec[2] * 9 * errState[11];
+    errstate = desstate - currstate;
+    u[1] = inertiadiagvec[0] * 81 * errstate[3] + 2 * inertiadiagvec[0] * 9 * errstate[9];
+    u[2] = inertiadiagvec[1] * 81 * errstate[4] + 2 * inertiadiagvec[1] * 9 * errstate[10];
+    u[3] = inertiadiagvec[2] * 81 * errstate[5] + 2 * inertiadiagvec[2] * 9 * errstate[11];
 
-    /** Motor Model for motor i:
+    /** motor model for motor i:
      **         time_constant_up = 0.0125 sec
      **         time_const_down = 0.025 sec
-     **         thrust(t) = thrust(t-1) + (controlThrust(t-1) - thrust(t-1)) * timeStep/time_constant **/
+     **         thrust(t) = thrust(t-1) + (controlthrust(t-1) - thrust(t-1)) * timestep/time_constant **/
     for (int i = 0; i<4; i++){
-        if (thrusts[i]<controlThrusts[i]) {  // time constant for increasing rotor speed
-            thrusts[i] = thrusts[i] + (controlThrusts[i] - thrusts[i]) * timeStep / 0.0125;
-        } else if (thrusts[i]>controlThrusts[i]){   // time constant for decreasing rotor speed
-            thrusts[i] = thrusts[i] + (controlThrusts[i] - thrusts[i]) * timeStep / 0.025;
+        if (thrusts[i]<controlthrusts[i]) {  // time constant for increasing rotor speed
+            thrusts[i] = thrusts[i] + (controlthrusts[i] - thrusts[i]) * timestep / 0.0125;
+        } else if (thrusts[i]>controlthrusts[i]){   // time constant for decreasing rotor speed
+            thrusts[i] = thrusts[i] + (controlthrusts[i] - thrusts[i]) * timestep / 0.025;
         }
     }
 
     /** the input u for reaching the the desired state has to be transformed into each rotor thrust.
-     ** The control thrust from this time step will be partly applied in the next time step
+     ** the control thrust from this time step will be partly applied in the next time step
      ** due to the rotor velocity change delay in the motor model **/
-    controlThrusts = thrusts2TorquesAndForces.inverse() * u;
+    controlthrusts = thrusts2torquesandforces.inverse() * u;
 
-    /// scale controlThrusts and avoid thrusts out of range: 0.5 - 1.5 * hoverThrust
-    double max_scale = controlThrusts.maxCoeff();
-    if (max_scale > 1.5 * hoverThrust){
-        controlThrusts = 1.5 / max_scale * hoverThrust * controlThrusts;
+    /// scale controlthrusts and avoid thrusts out of range: 0.5 - 1.5 * hoverthrust
+    double max_scale = controlthrusts.maxcoeff();
+    if (max_scale > 1.5 * hoverthrust){
+        controlthrusts = 1.5 / max_scale * hoverthrust * controlthrusts;
     }
     for (int i = 0; i<4; i++) {
-        if (controlThrusts[i] < 0.5 * hoverThrust) {
-            controlThrusts[i] = 0.5 * hoverThrust;
+        if (controlthrusts[i] < 0.5 * hoverthrust) {
+            controlthrusts[i] = 0.5 * hoverthrust;
         }
     }
 
